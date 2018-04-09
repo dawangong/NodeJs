d4b3bdd838436b33c203158b02fb6350e2dc4409
dawangong <dawangong@126.com>
2018-04-09 22:37:32 +0800

    update

---
+++ node-路由调用防出错.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import Router from './module/router';
+import url from 'url';
+
+http.createServer((request, response) => {
+    response.writeHead(200,{'content-type':'text/html;charset=utf-8'});
+    if (request.url !== '/favicon.ico'){
+        let rt = new Router();
+        let pathname = url.parse(request.url).pathname.replace(/\//, '');
+        typeof rt[pathname](response) === 'function' && rt[pathname](response);
+    }
+    response.end('');
+}).listen(4000);
+console.log('请访问地址:localhost:4000');
+
